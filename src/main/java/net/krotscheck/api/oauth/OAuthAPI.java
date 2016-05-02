abaefa35bbfbff9e90c6332071d846fce56cf6d6
Michael Krotscheck <krotscheck@gmail.com>
2016-05-02 05:52:04 -0700

    Initial commit of the OAuth API.
    Does nothing, except throw 404's.

---
+++ src/main/java/net/krotscheck/api/oauth/OAuthAPI.java
@@ -0,0 +1,50 @@
+/*
+ * Copyright (c) 2016 Michael Krotscheck
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not
+ * use this file except in compliance with the License. You may obtain a copy
+ * of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+ * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package net.krotscheck.api.oauth;
+
+import net.krotscheck.features.config.ConfigurationFeature;
+import net.krotscheck.features.database.DatabaseFeature;
+import net.krotscheck.features.exception.ExceptionFeature;
+import net.krotscheck.features.jackson.JacksonFeature;
+import net.krotscheck.features.version.VersionFeature;
+import org.glassfish.jersey.CommonProperties;
+import org.glassfish.jersey.server.ResourceConfig;
+
+/**
+ * The OAuth API, hosted at /oauth.
+ *
+ * @author Michael Krotscheck
+ */
+public class OAuthAPI extends ResourceConfig {
+
+    /**
+     * Constructor. Creates a new application instance.
+     */
+    public OAuthAPI() {
+        // No autodiscovery, we load everything explicitly.
+        property(CommonProperties.FEATURE_AUTO_DISCOVERY_DISABLE, true);
+
+        // Common features.
+        register(ConfigurationFeature.class);    // Configuration loader
+        register(JacksonFeature.class);          // Data Type de/serialization.
+        register(ExceptionFeature.class);        // Exception Mapping.
+        register(DatabaseFeature.class);         // Database Feature.
+        register(VersionFeature.class);          // Version response attachment.
+    }
+}
+
---
+++ src/test/java/net/krotscheck/api/oauth/OAuthAPITest.java
@@ -0,0 +1,57 @@
+/*
+ * Copyright (c) 2016 Michael Krotscheck
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not
+ * use this file except in compliance with the License. You may obtain a copy
+ * of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+ * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package net.krotscheck.api.oauth;
+
+import net.krotscheck.test.DatabaseTest;
+import org.apache.http.HttpStatus;
+import org.junit.Assert;
+import org.junit.Test;
+
+import javax.ws.rs.core.Application;
+import javax.ws.rs.core.Response;
+
+/**
+ * Smoke test that the OAuthAPI can be loaded.
+ *
+ * @author Michael Krotscheck
+ */
+public final class OAuthAPITest extends DatabaseTest {
+
+    /**
+     * Create a test instance of the application to test against.
+     *
+     * @return The constructed application.
+     */
+    @Override
+    protected Application configure() {
+        return new OAuthAPI();
+    }
+
+    /**
+     * The application smoketest.
+     */
+    @Test
+    public void smokeTest() {
+        Response response = target("/")
+                .request()
+                .get();
+
+        // Root should be 404
+        Assert.assertEquals(HttpStatus.SC_NOT_FOUND, response.getStatus());
+    }
+}
