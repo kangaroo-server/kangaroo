0799a016f8e735645191363154a1eb02d73b9679
Michael Krotscheck <krotscheck@gmail.com>
2016-06-14 09:40:15 -0700

    Added lots of utility methods to ContainerTest (#53)
    These methods assist with decoding URI query strings, following
    redirect requests, and building HTTP headers.

--- src/test/java/net/krotscheck/test/ContainerTest.java
+++ src/test/java/net/krotscheck/test/ContainerTest.java
@@ -17,6 +17,11 @@
 
 package net.krotscheck.test;
 
+import org.apache.commons.codec.binary.Base64;
+import org.apache.commons.io.IOUtils;
+import org.apache.http.HttpStatus;
+import org.apache.http.NameValuePair;
+import org.apache.http.client.utils.URLEncodedUtils;
 import org.glassfish.jersey.client.ClientConfig;
 import org.glassfish.jersey.client.ClientProperties;
 import org.glassfish.jersey.test.JerseyTest;
@@ -24,8 +29,23 @@ import org.hibernate.Session;
 import org.junit.After;
 import org.junit.AfterClass;
 import org.junit.BeforeClass;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 import java.io.File;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.StringWriter;
+import java.net.URI;
+import java.nio.charset.Charset;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.UUID;
+import javax.ws.rs.client.WebTarget;
+import javax.ws.rs.core.Response;
+
+import static org.junit.Assert.assertEquals;
 
 /**
  * A test suite sets up an entire application container to run our tests
@@ -34,6 +54,11 @@ import java.io.File;
  * @author Michael Krotscheck
  */
 public abstract class ContainerTest extends JerseyTest {
+
+    /**
+     * Logger instance.
+     */
+    private static Logger logger = LoggerFactory.getLogger(ContainerTest.class);
 
     /**
      * The database management instance.
@@ -96,5 +121,94 @@ public abstract class ContainerTest extends JerseyTest {
      */
     public static void loadTestData(final File testData) {
         manager.loadTestData(testData);
+    }
+
+    /**
+     * Helper method, which extracts the query string from a URI.
+     *
+     * @param uri The URI from which we're pulling the query response.
+     * @return A map of all responses.
+     */
+    protected final Map<String, String> parseQueryParams(final URI uri) {
+        return parseQueryParams(uri.getRawQuery());
+    }
+
+    /**
+     * Helper method, which extracts parameters from a query string.
+     *
+     * @param query The Query string to decode.
+     * @return A map of all responses.
+     */
+    protected final Map<String, String> parseQueryParams(final String query) {
+        List<NameValuePair> params = URLEncodedUtils
+                .parse(query, Charset.forName("ISO-8859-1"));
+
+        Map<String, String> map = new HashMap<>();
+        for (NameValuePair pair : params) {
+            map.put(pair.getName(), pair.getValue());
+        }
+        return map;
+    }
+
+    /**
+     * Helper method, which extracts the query string from a response body.
+     *
+     * @param response The HTTP query response to extract the body from.
+     * @return A map of all variables in the inputstream.
+     */
+    protected final Map<String, String> parseBodyParams(final Response
+                                                                response) {
+        try {
+            Charset charset = Charset.forName("UTF-8");
+            InputStream stream = (InputStream) response.getEntity();
+            StringWriter writer = new StringWriter();
+            IOUtils.copy(stream, writer, charset);
+            return parseQueryParams(writer.toString());
+        } catch (IOException ioe) {
+            logger.error("Could not decode params in stream.", ioe);
+        }
+        return new HashMap<>();
+    }
+
+    /**
+     * Build an authorization header.
+     *
+     * @param user     The user.
+     * @param password The password.
+     * @return "Basic [base64(user+":"+password)]
+     */
+    protected final String buildAuthorizationHeader(final UUID user,
+                                                    final String password) {
+        StringBuilder credBuilder = new StringBuilder();
+        credBuilder.append(user.toString()).append(":").append(password);
+        byte[] bytesEncoded =
+                Base64.encodeBase64(credBuilder.toString().getBytes());
+
+        StringBuilder authBuilder = new StringBuilder();
+        authBuilder.append("Basic ").append(new String(bytesEncoded));
+        return authBuilder.toString();
+    }
+
+    /**
+     * This is a convenience method which, presuming a response constitutes a
+     * redirect, will follow that redirect and return the subsequent response.
+     *
+     * @param original The original response, which should constitute a
+     *                 redirect.
+     * @return The response.
+     */
+    protected final Response followRedirect(final Response original) {
+
+        assertEquals(HttpStatus.SC_MOVED_TEMPORARILY, original.getStatus());
+
+        URI uri = original.getLocation();
+        WebTarget target = target().path(uri.getPath());
+
+        // Iterate through the query parameters and apply them.
+        for (NameValuePair pair : URLEncodedUtils.parse(uri, "UTF-8")) {
+            target = target.queryParam(pair.getName(), pair.getValue());
+        }
+
+        return target.request().get();
     }
 }
