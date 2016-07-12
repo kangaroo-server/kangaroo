005daf50bde8cd479a25c86c254d25c6e2bbfab3
Michael Krotscheck <krotscheck@gmail.com>
2016-07-12 16:38:28 +0200

    Database-backed configuration element for the admin servlet.
    This patch adds a factory which provides a database-backed
    singleton configuration instance for application-specific settings.
    Here we can store the default application ID, base URI's, and
    other defaults which may be handy.

---
+++ kangaroo-servlet-admin/src/test/java/net/krotscheck/kangaroo/servlet/admin/v1/config/AdminConfigurationFactoryTest.java
@@ -0,0 +1,132 @@
+/*
+ * Copyright (c) 2016 Michael Krotscheck
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not
+ * use this file except in compliance with the License. You may obtain a copy
+ * of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+ * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package net.krotscheck.kangaroo.servlet.admin.v1.config;
+
+import net.krotscheck.kangaroo.database.config.HibernateConfiguration;
+import net.krotscheck.kangaroo.servlet.admin.v1.config.AdminConfigurationFactory.Binder;
+import net.krotscheck.kangaroo.test.DatabaseTest;
+import net.krotscheck.kangaroo.test.IFixture;
+import org.apache.commons.configuration.Configuration;
+import org.glassfish.hk2.api.ActiveDescriptor;
+import org.glassfish.hk2.api.Factory;
+import org.glassfish.hk2.api.ServiceLocator;
+import org.glassfish.hk2.api.ServiceLocatorFactory;
+import org.glassfish.hk2.utilities.BuilderHelper;
+import org.glassfish.hk2.utilities.ServiceLocatorUtilities;
+import org.junit.Assert;
+import org.junit.Test;
+import org.mockito.Mockito;
+
+import java.util.List;
+import javax.inject.Singleton;
+
+/**
+ * Test that the admin configuration factory creates a singleton instance of
+ * the HibernateConfiguration.
+ */
+public final class AdminConfigurationFactoryTest extends DatabaseTest {
+
+    /**
+     * Load data fixtures for each test.
+     *
+     * @return A list of fixtures, which will be cleared after the test.
+     * @throws Exception An exception that indicates a failed fixture load.
+     */
+    @Override
+    public List<IFixture> fixtures() throws Exception {
+        return null;
+    }
+
+    /**
+     * Assert that we can create a configuration.
+     */
+    @Test
+    public void testCreateConfiguration() {
+        // Load some data.
+        HibernateConfiguration compareConfig =
+                new HibernateConfiguration(getSessionFactory(),
+                        AdminConfigurationFactory.GROUP_NAME);
+        compareConfig.addProperty("test", "property");
+
+        // Create a new instance from the factory.
+        AdminConfigurationFactory factory =
+                new AdminConfigurationFactory(getSessionFactory());
+        Configuration created = factory.provide();
+
+        // Make sure it can access the data.
+        Assert.assertEquals("property", created.getString("test"));
+    }
+
+    /**
+     * Assert that disposing doesn't really do anything.
+     */
+    @Test
+    public void testDispose() {
+        // Create a new instance from the factory.
+        AdminConfigurationFactory factory =
+                new AdminConfigurationFactory(getSessionFactory());
+        Configuration mock = Mockito.mock(Configuration.class);
+        factory.dispose(mock);
+
+        Mockito.verifyNoMoreInteractions(mock);
+    }
+
+    /**
+     * Assert that the component is bound properly.
+     */
+    @Test
+    public void testBinder() {
+        ServiceLocatorFactory factory = ServiceLocatorFactory.getInstance();
+        ServiceLocator locator = factory.create(getClass().getCanonicalName());
+
+        Binder b = new AdminConfigurationFactory.Binder();
+        ServiceLocatorUtilities.bind(locator, b);
+
+        List<ActiveDescriptor<?>> descriptors =
+                locator.getDescriptors(
+                        BuilderHelper.createNameAndContractFilter(
+                                Configuration.class.getName(),
+                                AdminConfigurationFactory.GROUP_NAME));
+        Assert.assertEquals(1, descriptors.size());
+
+        ActiveDescriptor descriptor = descriptors.get(0);
+        Assert.assertNotNull(descriptor);
+        // Check scope...
+        Assert.assertEquals(Singleton.class.getCanonicalName(),
+                descriptor.getScope());
+
+        // ... check name.
+        Assert.assertEquals(AdminConfigurationFactory.GROUP_NAME,
+                descriptor.getName());
+    }
+
+    /**
+     * Assert that the generic interface works as expected.
+     *
+     * @throws Exception Should not be thrown.
+     * @see <a href="https://sourceforge.net/p/cobertura/bugs/92/">https://sourceforge.net/p/cobertura/bugs/92/</a>
+     */
+    @Test
+    public void testGenericInterface() throws Exception {
+        // Intentionally using the generic untyped interface here.
+        Factory factory = new AdminConfigurationFactory(getSessionFactory());
+        Object instance = factory.provide();
+        Assert.assertTrue(instance instanceof HibernateConfiguration);
+        factory.dispose(instance);
+    }
+}
