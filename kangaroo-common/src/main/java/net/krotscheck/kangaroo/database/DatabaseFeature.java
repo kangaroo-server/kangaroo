c774f012fac3d9fdcef27e43c99174168a35c743
Michael Krotscheck <krotscheck@gmail.com>
2016-11-16 08:52:43 -0800

    Added exception mapper for hibernate constraints. (#160)
    Some database constraints are not thrown using the common
    ConstraintValidationException. This catches the rest.

--- kangaroo-common/src/main/java/net/krotscheck/kangaroo/database/DatabaseFeature.java
+++ kangaroo-common/src/main/java/net/krotscheck/kangaroo/database/DatabaseFeature.java
@@ -19,6 +19,7 @@ package net.krotscheck.kangaroo.database;
 
 import net.krotscheck.kangaroo.database.listener.CreatedUpdatedListener;
 import net.krotscheck.kangaroo.database.mapper.ConstraintViolationExceptionMapper;
+import net.krotscheck.kangaroo.database.mapper.HibernateConstraintExceptionMapper;
 import net.krotscheck.kangaroo.database.mapper.HibernateExceptionMapper;
 import net.krotscheck.kangaroo.database.mapper.PropertyValueExceptionMapper;
 import net.krotscheck.kangaroo.database.mapper.QueryExceptionMapper;
@@ -51,6 +52,7 @@ public final class DatabaseFeature implements Feature {
         context.register(new QueryExceptionMapper.Binder());
         context.register(new HibernateExceptionMapper.Binder());
         context.register(new ConstraintViolationExceptionMapper.Binder());
+        context.register(new HibernateConstraintExceptionMapper.Binder());
         context.register(new PropertyValueExceptionMapper.Binder());
         context.register(new SearchExceptionMapper.Binder());
 
