6bdde6cb4d905a143fe3bb9df2b80c1c93ce72ac
Michael Krotscheck <krotscheck@gmail.com>
2017-03-15 20:27:33 -0700

    Migrated Browse Tests to new ContainerTest
    This patch switches the browse tests over to the new
    ContainerTest, including the ApplicationBuilder. The search
    data will be loaded only once, which should speed things up.

--- kangaroo-server-admin/src/test/java/net/krotscheck/kangaroo/servlet/admin/v1/resource/UserServiceBrowseTest.java
+++ kangaroo-server-admin/src/test/java/net/krotscheck/kangaroo/servlet/admin/v1/resource/UserServiceBrowseTest.java
@@ -51,7 +51,7 @@ import java.util.stream.Collectors;
  */
 @RunWith(Parameterized.class)
 public final class UserServiceBrowseTest
-        extends DAbstractServiceBrowseTest<User> {
+        extends AbstractServiceBrowseTest<User> {
 
     /**
      * Generic type declaration for list decoding.
@@ -115,8 +115,9 @@ public final class UserServiceBrowseTest
     protected List<User> getAccessibleEntities(final OAuthToken token) {
         // If you're an admin, you get to see everything. If you're not, you
         // only get to see what you own.
-        if (!token.getScopes().containsKey(getAdminScope())) {
-            return getOwnedEntities(token);
+        OAuthToken attachedToken = getAttached(token);
+        if (!attachedToken.getScopes().containsKey(getAdminScope())) {
+            return getOwnedEntities(attachedToken);
         }
 
         // We know you're an admin. Get all applications in the system.
