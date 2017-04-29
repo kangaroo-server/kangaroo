8cb875ed34b98b1b060993b455b48f987a01a38e
Michael Krotscheck <krotscheck@gmail.com>
2017-04-28 20:30:20 -0700

    Added common configuration options (#258)
    A list of common commandline configuration parameters.

---
+++ kangaroo-common/src/main/java/net/krotscheck/kangaroo/server/Config.java
@@ -0,0 +1,52 @@
+/*
+ * Copyright (c) 2017 Michael Krotscheck
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not
+ * use this file except in compliance with the License. You may obtain a copy
+ * of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+ * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ */
+
+package net.krotscheck.kangaroo.server;
+
+import java.util.AbstractMap.SimpleImmutableEntry;
+import java.util.Map.Entry;
+
+/**
+ * List of configuration settings, and their defaults.
+ *
+ * @author Michael Krotscheck
+ */
+public final class Config {
+
+    /**
+     * Configuration key, and default, for the IP address to which this
+     * server will bind itself.
+     */
+    @SuppressWarnings("PMD.AvoidUsingHardCodedIP")
+    public static final Entry<String, String> HOST = new
+            SimpleImmutableEntry<>("kangaroo.host", "127.0.0.1");
+
+    /**
+     * Configuration key, and default, for the port on which this server will
+     * listen.
+     */
+    public static final Entry<String, Integer> PORT = new
+            SimpleImmutableEntry<>("kangaroo.port", 8080);
+
+    /**
+     * Private constructor for a utility class.
+     */
+    private Config() {
+
+    }
+}
