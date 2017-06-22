e592ea909ca5687d1f3db4a8a248addca30d8534
Michael Krotscheck <krotscheck@gmail.com>
2017-06-21 18:43:16 -0700

    Add "Powered-By" header.
    This patch adds the "Powered-By: Kangaroo" header.

---
+++ kangaroo-common/src/main/java/net/krotscheck/kangaroo/common/status/PoweredByFilter.java
@@ -0,0 +1,65 @@
+/*
+ * Copyright (c) 2017 Michael Krotscheck
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not
+ * use this file except in compliance with the License. You may obtain a copy
+ * of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+ * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ */
+
+package net.krotscheck.kangaroo.common.status;
+
+import com.google.common.net.HttpHeaders;
+import org.glassfish.hk2.utilities.binding.AbstractBinder;
+
+import javax.inject.Singleton;
+import javax.ws.rs.container.ContainerRequestContext;
+import javax.ws.rs.container.ContainerResponseContext;
+import javax.ws.rs.container.ContainerResponseFilter;
+import javax.ws.rs.container.PreMatching;
+import java.io.IOException;
+
+/**
+ * This filter only adds the "Powered by" filter.
+ *
+ * @author Michael Krotscheck
+ */
+@PreMatching
+public final class PoweredByFilter implements ContainerResponseFilter {
+
+    /**
+     *
+     *
+     * @param requestContext  request context.
+     * @param responseContext response context.
+     * @throws IOException if an I/O exception occurs.
+     */
+    @Override
+    public void filter(final ContainerRequestContext requestContext,
+                       final ContainerResponseContext responseContext) throws
+            IOException {
+        responseContext.getHeaders().add(HttpHeaders.X_POWERED_BY, "Kangaroo");
+    }
+
+    /**
+     * HK2 Binder for our injector context.
+     */
+    public static final class Binder extends AbstractBinder {
+
+        @Override
+        protected void configure() {
+            bind(PoweredByFilter.class)
+                    .to(ContainerResponseFilter.class)
+                    .in(Singleton.class);
+        }
+    }
+}
---
+++ kangaroo-common/src/test/java/net/krotscheck/kangaroo/common/status/PoweredByFilterTest.java
@@ -0,0 +1,84 @@
+/*
+ * Copyright (c) 2017 Michael Krotscheck
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not
+ * use this file except in compliance with the License. You may obtain a copy
+ * of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+ * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ */
+
+package net.krotscheck.kangaroo.common.status;
+
+import com.google.common.net.HttpHeaders;
+import net.krotscheck.kangaroo.test.KangarooJerseyTest;
+import org.glassfish.jersey.server.ResourceConfig;
+import org.junit.Assert;
+import org.junit.Test;
+
+import javax.ws.rs.GET;
+import javax.ws.rs.Path;
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.Response;
+
+/**
+ * Test the "Powered-By" filter.
+ *
+ * @author Michael Krotscheck
+ */
+public final class PoweredByFilterTest extends KangarooJerseyTest {
+
+    /**
+     * Build an application.
+     *
+     * @return A configured application.
+     */
+    @Override
+    protected ResourceConfig createApplication() {
+        ResourceConfig config = new ResourceConfig();
+        config.register(PoweredByFilter.class);
+        config.register(MockService.class);
+        return config;
+    }
+
+    /**
+     * Assert that the jackson feature is available.
+     */
+    @Test
+    public void testFilters() {
+        Response r = target("/").request().get();
+
+        Assert.assertEquals("Kangaroo",
+                r.getHeaderString(HttpHeaders.X_POWERED_BY));
+    }
+
+    /**
+     * A simple endpoint.
+     *
+     * @author Michael Krotscheck
+     */
+    @Path("/")
+    public static final class MockService {
+
+        /**
+         * Return OK.
+         *
+         * @return Nothing, error thrown.
+         */
+        @GET
+        @Produces(MediaType.APPLICATION_JSON)
+        public Response modifyPojo() {
+            return Response.ok().build();
+        }
+
+    }
+}
