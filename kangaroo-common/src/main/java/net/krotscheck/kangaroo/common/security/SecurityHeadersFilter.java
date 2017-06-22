c9e246469dfb204ce3b25b804286f508dd658bb3
Michael Krotscheck <krotscheck@gmail.com>
2017-06-22 06:17:53 -0700

    Created SecurityFeature (#283)
    While currently small, this feature will contain security checks necessary
    for all API's.

---
+++ kangaroo-common/src/main/java/net/krotscheck/kangaroo/common/security/SecurityHeadersFilter.java
@@ -0,0 +1,67 @@
+/*
+ * Copyright (c) 2017 Michael Krotscheck
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not
+ * use this file except in compliance with the License. You may obtain a copy
+ * of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+ * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ */
+
+package net.krotscheck.kangaroo.common.security;
+
+import com.google.common.net.HttpHeaders;
+import org.glassfish.hk2.utilities.binding.AbstractBinder;
+
+import javax.inject.Singleton;
+import javax.ws.rs.container.ContainerRequestContext;
+import javax.ws.rs.container.ContainerResponseContext;
+import javax.ws.rs.container.ContainerResponseFilter;
+import javax.ws.rs.container.PreMatching;
+import java.io.IOException;
+
+/**
+ * HTTP headers which need to decorate most request responses.
+ *
+ * @author Michael Krotscheck
+ */
+@PreMatching
+public final class SecurityHeadersFilter implements ContainerResponseFilter {
+
+    /**
+     * Decorate the response with any security-related headers that we need.
+     *
+     * @param requestContext  request context.
+     * @param responseContext response context.
+     * @throws IOException if an I/O exception occurs.
+     */
+    @Override
+    public void filter(final ContainerRequestContext requestContext,
+                       final ContainerResponseContext responseContext) throws
+            IOException {
+
+        // Not strictly necessary for API's, but might as well.
+        responseContext.getHeaders().add(HttpHeaders.X_FRAME_OPTIONS, "Deny");
+    }
+
+    /**
+     * HK2 Binder for our injector context.
+     */
+    public static final class Binder extends AbstractBinder {
+
+        @Override
+        protected void configure() {
+            bind(SecurityHeadersFilter.class)
+                    .to(ContainerResponseFilter.class)
+                    .in(Singleton.class);
+        }
+    }
+}
