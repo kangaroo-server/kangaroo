70cb0411be24d853307c63153c4b455679dee9ef
Michael Krotscheck <krotscheck@gmail.com>
2017-08-23 07:17:32 -0700

    Configuration service (#310)
    This service exposes client-required configuration via the API.

---
+++ kangaroo-server-authz/src/test/java/net/krotscheck/kangaroo/authz/admin/v1/resource/ConfigServiceTest.java
@@ -0,0 +1,107 @@
+/*
+ * Copyright (c) 2017 Michael Krotscheck
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not
+ * use this file except in compliance with the License. You may obtain a copy
+ * of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+ * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ */
+
+package net.krotscheck.kangaroo.authz.admin.v1.resource;
+
+import net.krotscheck.kangaroo.authz.admin.v1.resource.ConfigService.ConfigurationEntity;
+import net.krotscheck.kangaroo.authz.admin.v1.servlet.Config;
+import net.krotscheck.kangaroo.authz.common.database.entity.AbstractAuthzEntity;
+import net.krotscheck.kangaroo.authz.common.database.entity.Application;
+import org.apache.commons.configuration.Configuration;
+import org.hibernate.Session;
+import org.junit.Assert;
+import org.junit.Test;
+
+import javax.ws.rs.core.Response;
+import java.net.URI;
+import java.util.UUID;
+
+import static org.junit.Assert.assertTrue;
+
+/**
+ * Unit tests for the configuration service.
+ */
+public final class ConfigServiceTest extends AbstractResourceTest {
+
+    /**
+     * This service is world accessible.
+     *
+     * @return null.
+     */
+    @Override
+    protected String getAdminScope() {
+        return null;
+    }
+
+    /**
+     * This service is world accessible.
+     *
+     * @return null.
+     */
+    @Override
+    protected String getRegularScope() {
+        return null;
+    }
+
+    /**
+     * There is no id on this service.
+     *
+     * @return null.
+     */
+    @Override
+    protected URI getUrlForId(final String id) {
+        return null;
+    }
+
+    /**
+     * There are no entities in this service.
+     *
+     * @return null.
+     */
+    @Override
+    protected URI getUrlForEntity(final AbstractAuthzEntity entity) {
+        return null;
+    }
+
+    /**
+     * A test.
+     */
+    @Test
+    public void testBasicUsage() {
+        Session session = getSession();
+        Configuration config = getSystemConfig();
+        UUID applicationId = UUID.fromString(config
+                .getString(Config.APPLICATION_ID));
+        UUID clientId = UUID.fromString(config
+                .getString(Config.APPLICATION_CLIENT_ID));
+
+        Application a = session.get(Application.class, applicationId);
+
+        Response r = target("/config")
+                .request()
+                .get();
+
+        Assert.assertEquals(200, r.getStatus());
+
+        ConfigurationEntity entity = r.readEntity(ConfigurationEntity.class);
+
+        a.getScopes().keySet()
+                .forEach(s -> assertTrue(entity.getScopes().contains(s)));
+        Assert.assertEquals(entity.getClient(), clientId);
+    }
+}
