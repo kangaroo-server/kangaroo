082f88e98a32ca65acf5cf1d48d0fbde3c28c2d3
Michael Krotscheck <krotscheck@gmail.com>
2017-11-23 12:31:22 -0800

    Added more resilient checks to googla and facebook auth tests.
    These checks include valid flows for google's security
    challenges as well as individual account selection. We now use
    example.com for proper redirection resolution as some ISP's fiddle
    with DNS resultion failures, and common code has been moved into
    a single abstract test class.

--- kangaroo-server-authz/src/test/java/net/krotscheck/kangaroo/authz/common/authenticator/AbstractBrowserLoginTest.java
+++ kangaroo-server-authz/src/test/java/net/krotscheck/kangaroo/authz/common/authenticator/AbstractBrowserLoginTest.java
@@ -16,26 +16,19 @@
  *
  */
 
-package net.krotscheck.kangaroo.authz.common.authenticator.google;
+package net.krotscheck.kangaroo.authz.common.authenticator;
 
-import net.krotscheck.kangaroo.authz.common.authenticator.AuthenticatorType;
 import net.krotscheck.kangaroo.authz.common.authenticator.oauth2.AbstractOAuth2Authenticator;
 import net.krotscheck.kangaroo.authz.common.database.entity.ClientType;
-import net.krotscheck.kangaroo.authz.common.database.entity.OAuthToken;
-import net.krotscheck.kangaroo.authz.common.database.entity.OAuthTokenType;
 import net.krotscheck.kangaroo.authz.oauth2.OAuthAPI;
 import net.krotscheck.kangaroo.authz.test.ApplicationBuilder;
 import net.krotscheck.kangaroo.authz.test.ApplicationBuilder.ApplicationContext;
-import net.krotscheck.kangaroo.common.hibernate.id.IdUtil;
 import net.krotscheck.kangaroo.test.TestConfig;
 import net.krotscheck.kangaroo.test.jersey.ContainerTest;
 import net.krotscheck.kangaroo.test.jersey.SingletonTestContainerFactory;
-import net.krotscheck.kangaroo.test.rule.FacebookTestUser;
 import net.krotscheck.kangaroo.test.rule.SeleniumRule;
 import net.krotscheck.kangaroo.test.rule.TestDataResource;
 import net.krotscheck.kangaroo.test.runner.SingleInstanceTestRunner;
-import net.krotscheck.kangaroo.util.HttpUtil;
-import org.apache.commons.lang.RandomStringUtils;
 import org.glassfish.jersey.server.ResourceConfig;
 import org.glassfish.jersey.servlet.ServletContainer;
 import org.glassfish.jersey.test.DeploymentContext;
@@ -44,33 +37,22 @@ import org.glassfish.jersey.test.TestProperties;
 import org.glassfish.jersey.test.spi.TestContainerException;
 import org.glassfish.jersey.test.spi.TestContainerFactory;
 import org.hibernate.Session;
-import org.junit.After;
-import org.junit.Before;
 import org.junit.ClassRule;
-import org.junit.Test;
 import org.junit.rules.TestRule;
 import org.junit.runner.RunWith;
-import org.openqa.selenium.By;
-import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.support.ui.ExpectedConditions;
-import org.openqa.selenium.support.ui.WebDriverWait;
 
-import javax.ws.rs.core.MultivaluedMap;
-import javax.ws.rs.core.UriBuilder;
-import java.math.BigInteger;
-import java.net.URI;
 import java.util.HashMap;
 import java.util.Map;
 
-import static org.junit.Assert.assertEquals;
-
 /**
- * This test performs a series of full login flows against facebook.
+ * An abstract testing class that sets up the browser harness and a running
+ * application instance. Also includes all runtime data, for an application
+ * that contains one-of-each login method.
  *
  * @author Michael Krotscheck
  */
 @RunWith(SingleInstanceTestRunner.class)
-public final class GoogleFullAuthFlowTest extends ContainerTest {
+public abstract class AbstractBrowserLoginTest extends ContainerTest {
 
     /**
      * The selenium rule.
@@ -79,9 +61,23 @@ public final class GoogleFullAuthFlowTest extends ContainerTest {
     public static final SeleniumRule SELENIUM = new SeleniumRule();
 
     /**
+     * Timeout, in seconds, used to wait for specific driver changes.
+     */
+    protected static final Integer TIMEOUT = 20;
+
+    /**
      * The test context for a regular application.
      */
     private static ApplicationContext context;
+
+    /**
+     * Get the application data context under test.
+     *
+     * @return The data snapshot for the current testing application.
+     */
+    public static ApplicationContext getContext() {
+        return context;
+    }
 
     /**
      * Test data loading for this test.
@@ -94,11 +90,18 @@ public final class GoogleFullAuthFlowTest extends ContainerTest {
                  */
                 @Override
                 protected void loadTestData(final Session session) {
-                    Map<String, String> config = new HashMap<>();
-                    config.put(AbstractOAuth2Authenticator.CLIENT_ID_KEY,
+
+                    Map<String, String> googleConfig = new HashMap<>();
+                    googleConfig.put(AbstractOAuth2Authenticator.CLIENT_ID_KEY,
                             TestConfig.getGoogleAppId());
-                    config.put(AbstractOAuth2Authenticator.CLIENT_SECRET_KEY,
+                    googleConfig.put(AbstractOAuth2Authenticator.CLIENT_SECRET_KEY,
                             TestConfig.getGoogleAppSecret());
+
+                    Map<String, String> fbConfig = new HashMap<>();
+                    fbConfig.put(AbstractOAuth2Authenticator.CLIENT_ID_KEY,
+                            TestConfig.getFacebookAppId());
+                    fbConfig.put(AbstractOAuth2Authenticator.CLIENT_SECRET_KEY,
+                            TestConfig.getFacebookAppSecret());
 
                     context = ApplicationBuilder
                             .newApplication(session)
@@ -106,8 +109,9 @@ public final class GoogleFullAuthFlowTest extends ContainerTest {
                             .scope("debug1")
                             .role("test", new String[]{"debug", "debug1"})
                             .client(ClientType.AuthorizationGrant)
-                            .authenticator(AuthenticatorType.Google, config)
-                            .redirect("http://valid.example.com/redirect")
+                            .authenticator(AuthenticatorType.Google, googleConfig)
+                            .authenticator(AuthenticatorType.Facebook, fbConfig)
+                            .redirect("https://www.example.com/")
                             .build();
                 }
             };
@@ -120,49 +124,6 @@ public final class GoogleFullAuthFlowTest extends ContainerTest {
      * The current running test application.
      */
     private ResourceConfig testApplication;
-
-    /**
-     * Reset the facebook account before every test.
-     */
-    @Before
-    public void googleLogin() {
-        WebDriver d = SELENIUM.getDriver();
-
-        By emailInput = By.id("identifierId");
-        By nextButton = By.id("identifierNext");
-        By passInput = By.cssSelector("div#password input[type='password']");
-        By passNext = By.id("passwordNext");
-
-        // Login to facebook.
-        d.get("https://accounts.google.com/ServiceLogin");
-        new WebDriverWait(d, 10)
-                .until(ExpectedConditions.presenceOfElementLocated(emailInput));
-
-        // Enter the login
-        d.findElement(emailInput).clear();
-        d.findElement(emailInput).sendKeys(TestConfig.getGoogleAccountId());
-        d.findElement(nextButton).click();
-        new WebDriverWait(d, 10)
-                .until(ExpectedConditions.presenceOfElementLocated(passInput));
-
-        // Enter the password
-        d.findElement(passInput).clear();
-        d.findElement(passInput).sendKeys(TestConfig.getGoogleAccountSecret());
-        d.findElement(passNext).click();
-        new WebDriverWait(d, 10)
-                .until(ExpectedConditions.urlContains("https://myaccount.google.com"));
-    }
-
-    /**
-     * Reset the google account before every test.
-     */
-    @After
-    public void googleLogout() {
-        WebDriver d = SELENIUM.getDriver();
-        d.get("https://accounts.google.com/Logout");
-        new WebDriverWait(d, 10)
-                .until(ExpectedConditions.urlContains("https://accounts.google.com/ServiceLogin/signinchooser"));
-    }
 
     /**
      * This method overrides the underlying default test container provider,
@@ -214,50 +175,5 @@ public final class GoogleFullAuthFlowTest extends ContainerTest {
         return ServletDeploymentContext.forServlet(
                 new ServletContainer(createApplication()))
                 .build();
-    }
-
-    /**
-     * 1. Someone new to your app logs in with Facebook
-     * - Go to your app and tap on the Log in with Facebook button
-     * - Tap OK to accept the read permissions
-     * - Click OK again to accept write permissions if applicable
-     * - Go to app settings and verify that the granted permissions are there
-     */
-    @Test
-    public void testNewLogin() {
-        String testState = RandomStringUtils.randomAlphanumeric(20);
-
-        // Issue a request against our /authorize endpoint.
-        URI requestUri = UriBuilder.fromUri(getBaseUri())
-                .path("/authorize")
-                .queryParam("response_type", "code")
-                .queryParam("client_id",
-                        IdUtil.toString(context.getClient().getId()))
-                .queryParam("scope", "debug")
-                .queryParam("state", testState)
-                .build();
-
-        WebDriver d = SELENIUM.getDriver();
-        d.get(requestUri.toString());
-        (new WebDriverWait(d, 10))
-                .until(ExpectedConditions.elementToBeClickable(By.id("submit_approve_access")));
-        d.findElement(By.id("submit_approve_access")).click();
-
-        (new WebDriverWait(d, 10))
-                .until(ExpectedConditions.urlContains("valid.example.com"));
-
-        String url = d.getCurrentUrl();
-        URI uri = URI.create(url);
-        MultivaluedMap<String, String> params = HttpUtil.parseQueryParams(uri);
-        BigInteger code = IdUtil.fromString(params.getFirst("code"));
-        assertEquals(testState, params.getFirst("state"));
-
-        OAuthToken authToken = getSession().get(OAuthToken.class, code);
-        assertEquals(authToken.getClient().getId(),
-                context.getClient().getId());
-        assertEquals(AuthenticatorType.Google,
-                authToken.getIdentity().getType());
-        assertEquals(OAuthTokenType.Authorization,
-                authToken.getTokenType());
     }
 }
---
+++ kangaroo-server-authz/src/test/java/net/krotscheck/kangaroo/authz/common/authenticator/GoogleFullAuthFlowTest.java
@@ -0,0 +1,207 @@
+/*
+ * Copyright (c) 2017 Michael Krotscheck
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not
+ * use this file except in compliance with the License. You may obtain a copy
+ * of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+ * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ */
+
+package net.krotscheck.kangaroo.authz.common.authenticator;
+
+import net.krotscheck.kangaroo.authz.common.database.entity.OAuthToken;
+import net.krotscheck.kangaroo.authz.common.database.entity.OAuthTokenType;
+import net.krotscheck.kangaroo.common.hibernate.id.IdUtil;
+import net.krotscheck.kangaroo.test.TestConfig;
+import net.krotscheck.kangaroo.test.runner.SingleInstanceTestRunner;
+import net.krotscheck.kangaroo.util.HttpUtil;
+import org.apache.commons.lang.RandomStringUtils;
+import org.glassfish.jersey.servlet.ServletContainer;
+import org.glassfish.jersey.test.DeploymentContext;
+import org.glassfish.jersey.test.ServletDeploymentContext;
+import org.glassfish.jersey.test.TestProperties;
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+import javax.ws.rs.core.MultivaluedMap;
+import javax.ws.rs.core.UriBuilder;
+import java.math.BigInteger;
+import java.net.URI;
+
+import static org.junit.Assert.assertEquals;
+import static org.openqa.selenium.support.ui.ExpectedConditions.elementToBeClickable;
+import static org.openqa.selenium.support.ui.ExpectedConditions.or;
+import static org.openqa.selenium.support.ui.ExpectedConditions.presenceOfElementLocated;
+import static org.openqa.selenium.support.ui.ExpectedConditions.urlContains;
+
+/**
+ * This test performs a series of full login flows against google.
+ *
+ * @author Michael Krotscheck
+ */
+@RunWith(SingleInstanceTestRunner.class)
+public final class GoogleFullAuthFlowTest
+        extends AbstractBrowserLoginTest {
+
+    /**
+     * Reset the facebook account before every test.
+     */
+    @Before
+    public void googleLogin() {
+        WebDriver d = SELENIUM.getDriver();
+
+        By emailInput = By.id("identifierId");
+        By nextButton = By.id("identifierNext");
+        By passInput = By.cssSelector("div#password input[type='password']");
+        By passNext = By.id("passwordNext");
+
+        // Login to google.
+        d.get("https://accounts.google.com/ServiceLogin");
+        SELENIUM.screenshot();
+        new WebDriverWait(d, 10)
+                .until(elementToBeClickable(emailInput));
+
+        // Enter the login
+        d.findElement(emailInput).clear();
+        d.findElement(emailInput).sendKeys(TestConfig.getGoogleAccountId());
+        d.findElement(nextButton).click();
+        SELENIUM.screenshot();
+        new WebDriverWait(d, TIMEOUT)
+                .until(elementToBeClickable(passInput));
+
+        // Enter the password
+        d.findElement(passInput).clear();
+        d.findElement(passInput).sendKeys(TestConfig.getGoogleAccountSecret());
+        d.findElement(passNext).click();
+
+        // Test for different conditions here.
+        SELENIUM.screenshot();
+        new WebDriverWait(d, TIMEOUT)
+                .until(or(
+                        urlContains("https://myaccount.google.com"),
+                        urlContains("https://accounts.google.com/signin/oauth/oauthchooseaccount"),
+                        urlContains("https://accounts.google.com/signin/v2/challenge/selection")
+                ));
+
+        // Test for the sign-in challenge
+        if (d.getCurrentUrl().contains("https://accounts.google.com/signin/v2/challenge/selection")) {
+            SELENIUM.screenshot();
+            d.findElement(By.cssSelector("[data-challengetype=16]")).click();
+
+            new WebDriverWait(d, TIMEOUT)
+                    .until(presenceOfElementLocated(
+                            By.id("knowledgeLoginLocationInput")
+                    ))
+                    .sendKeys("Bellevue, WA");
+
+            d.findElement(By.id("next")).click();
+
+            new WebDriverWait(d, TIMEOUT)
+                    .until(or(
+                            urlContains("https://myaccount.google.com")
+                    ));
+        }
+    }
+
+    /**
+     * Reset the google account before every test.
+     */
+    @After
+    public void googleLogout() {
+        WebDriver d = SELENIUM.getDriver();
+        d.get("https://accounts.google.com/Logout");
+        SELENIUM.screenshot();
+        new WebDriverWait(d, TIMEOUT)
+                .until(urlContains("https://accounts.google.com/"));
+        SELENIUM.screenshot();
+    }
+
+    /**
+     * Configure the deployment as a servlet.
+     *
+     * @return The deployment context.
+     */
+    @Override
+    protected DeploymentContext configureDeployment() {
+        // This matches the port registered with facebook.
+        forceSet(TestProperties.CONTAINER_PORT, TestConfig.getTestingPort());
+        forceSet(TestProperties.LOG_TRAFFIC, "true");
+        forceSet(TestProperties.DUMP_ENTITY, "true");
+
+        return ServletDeploymentContext.forServlet(
+                new ServletContainer(createApplication()))
+                .build();
+    }
+
+    /**
+     * Someone new to your app logs in with Google.
+     */
+    @Test
+    public void testNewLogin() {
+        String testState = RandomStringUtils.randomAlphanumeric(20);
+
+        // Issue a request against our /authorize endpoint.
+        URI requestUri = UriBuilder.fromUri(getBaseUri())
+                .path("/authorize")
+                .queryParam("authenticator", AuthenticatorType.Google)
+                .queryParam("response_type", "code")
+                .queryParam("client_id",
+                        IdUtil.toString(getContext().getClient().getId()))
+                .queryParam("scope", "debug")
+                .queryParam("state", testState)
+                .build();
+
+        WebDriver d = SELENIUM.getDriver();
+        d.get(requestUri.toString());
+        SELENIUM.screenshot();
+        (new WebDriverWait(d, TIMEOUT))
+                .until(or(
+                        urlContains("https://accounts.google.com/signin/oauth/oauthchooseaccount"),
+                        elementToBeClickable(By.id("submit_approve_access"))
+                ));
+        SELENIUM.screenshot();
+
+        // Test for the account chooser.
+        if (d.getCurrentUrl().contains("https://accounts.google" +
+                ".com/signin/oauth/oauthchooseaccount")) {
+            d.findElement(By.cssSelector("[data-profileindex=\"0\"]"))
+                    .click();
+            SELENIUM.screenshot();
+        } else {
+            d.findElement(By.id("submit_approve_access")).click();
+            SELENIUM.screenshot();
+        }
+
+        (new WebDriverWait(d, TIMEOUT))
+                .until(urlContains("www.example.com"));
+        SELENIUM.screenshot();
+
+        String url = d.getCurrentUrl();
+        URI uri = URI.create(url);
+        MultivaluedMap<String, String> params = HttpUtil.parseQueryParams(uri);
+        BigInteger code = IdUtil.fromString(params.getFirst("code"));
+        assertEquals(testState, params.getFirst("state"));
+
+        OAuthToken authToken = getSession().get(OAuthToken.class, code);
+        assertEquals(authToken.getClient().getId(),
+                getContext().getClient().getId());
+        assertEquals(AuthenticatorType.Google,
+                authToken.getIdentity().getType());
+        assertEquals(OAuthTokenType.Authorization,
+                authToken.getTokenType());
+    }
+}
