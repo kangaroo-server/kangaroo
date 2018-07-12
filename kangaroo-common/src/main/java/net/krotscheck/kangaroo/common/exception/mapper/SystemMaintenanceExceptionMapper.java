8b71eb416b7db8acd10467cefd57dc0a6b2c84c5
Michael Krotscheck <krotscheck@gmail.com>
2018-07-11 23:36:39 -0700

    System Maintenance Exception (#462)
    An exception that may be thrown when the system is in a maintenance state,
    such as when the database is being migrated.

---
+++ kangaroo-common/src/main/java/net/krotscheck/kangaroo/common/exception/mapper/SystemMaintenanceExceptionMapper.java
@@ -0,0 +1,68 @@
+/*
+ * Copyright (c) 2018 Michael Krotscheck
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not
+ * use this file except in compliance with the License. You may obtain a copy
+ * of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+ * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ */
+
+package net.krotscheck.kangaroo.common.exception.mapper;
+
+import net.krotscheck.kangaroo.common.exception.ErrorResponseBuilder;
+import net.krotscheck.kangaroo.common.exception.SystemMaintenanceException;
+import org.glassfish.jersey.internal.inject.AbstractBinder;
+
+import javax.inject.Singleton;
+import javax.ws.rs.core.HttpHeaders;
+import javax.ws.rs.core.Response;
+import javax.ws.rs.ext.ExceptionMapper;
+import javax.ws.rs.ext.Provider;
+
+/**
+ * Exception mapper for the system's maintenance mode. Maintenance retry is
+ * set at one minute.
+ *
+ * @author Michael Krotscheck
+ */
+@Provider
+public final class SystemMaintenanceExceptionMapper
+        implements ExceptionMapper<SystemMaintenanceException> {
+
+    /**
+     * Convert to response.
+     *
+     * @param e The exceptions to handle.
+     * @return The Response instance for this error.
+     */
+    public Response toResponse(final SystemMaintenanceException e) {
+        // Retry after 2 minutes.
+        return ErrorResponseBuilder
+                .from(e)
+                .addHeader(HttpHeaders.RETRY_AFTER, "60")
+                .build();
+
+    }
+
+    /**
+     * HK2 Binder for our injector context.
+     */
+    public static final class Binder extends AbstractBinder {
+
+        @Override
+        protected void configure() {
+            bind(SystemMaintenanceExceptionMapper.class)
+                    .to(ExceptionMapper.class)
+                    .in(Singleton.class);
+        }
+    }
+}
